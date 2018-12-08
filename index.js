588ec1ee7abdd96cee25ac281cc683f2a3f214e7
unknown <kyleteeter12@gmail.com>
2018-12-08 12:00:01 -0600

    Index.js file created.

--- index.js
+++ index.js
@@ -6,4 +6,21 @@ const logger = require('morgan')
 const server = express()
 const PORT = 4400;
 
-const actionRouter = require('./routers/action_router')
+const actionRouter = require('./routers/action_router');
+
+const projectRouter = require('./routers/project_router');
+
+server.use(
+    express.json(),
+    helmet(),
+    logger('tiny'),
+);
+
+server.use(express.json());
+server.use('/actions', actionRouter);
+server.use('/projects', projectRouter);
+
+// Listening
+server.listen(PORT, () => {
+    console.log(`Server is running and listening to ${PORT}`)
+})
