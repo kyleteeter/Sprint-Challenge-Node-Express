b64560b2e1cf49d00de9a3271f45ad3aa422e74b
unknown <kyleteeter12@gmail.com>
2018-12-08 12:52:49 -0600

    Complete Post for both action and projects.

--- routers/action_router.js
+++ routers/action_router.js
@@ -1,2 +1,50 @@
 const express = require('express');
 const actionModel = require('../data/helpers/actionModel')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    actionModel.get()
+        .then(actions => res.json(actions))
+        .catch(err =>
+            res.status(500)
+            .json({error: "The action coould not be retrieved."}))
+})
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+
+    actionModel.get(id)
+        .then((action) => {
+            if (action) {
+                res.json(action);
+            }else {
+                res.status(400)
+                .json({message: `The action with ID of ${id} does not exist.`})
+            }
+            res.json(action)
+        })
+        .catch(err =>
+            res.status(500)
+            .json({error: "The action info could not be retrieved."})
+        )
+})
+
+router.post('/', (req, res) => {
+    const action = req.body;
+
+    if (action.project_id && action.description && action.notes) {
+        actionModel.insert(action).then(actionID => {
+            actionModel.get(actionID.id).then(action => {
+                res.status(201).json(action);
+            })
+        }) .catch(err => {
+            res.status(500)
+            .json({error: `Error occured when saving ${action} to the database`})
+        })
+    } else {
+        res.status(400).json({error: "Please provide project ID and description."})
+    }
+})
+
+module.exports = router;
--- routers/project_router.js
+++ routers/project_router.js
@@ -1,2 +1,50 @@
 const express = require('express');
 const projectModel = require('../data/helpers/projectModel')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    projectModel.get()
+        .then(projects => res.json(projects))
+        .catch(err =>
+            res.status(500)
+            .json({error: "The action coould not be retrieved."}))
+})
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+    projectModel.get()
+        .then((project) => {
+            if (project) {
+                res.json(project)
+            } else {
+                res.status(400)
+                .json({message: `The project with ID of ${id} does not exist.`})
+            }
+            res.json(project)
+        })
+        .catch(err => 
+            res.status(500)
+            .json({error: "The project info could not be found."}))
+
+})
+
+router.post('/', (req, res) => {
+    const project = req.body;
+
+    if (project.name && project.description) {
+        projectModel.insert(project).then(projectID => {
+            projectModel.get(projectID.id).then(project => {
+                res.status(201).json(project);
+            })
+        }) .catch(err => {
+            res.status(500)
+            .json({error: `Error occured when saving ${project} to the database`})
+        })
+    } else {
+        res.status(400).json({error: "Please provide name and description."})
+    }
+})
+
+
+module.exports = router;
